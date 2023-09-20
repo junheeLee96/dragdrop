601a89f1d9280d7063a2663bd2b61d0a85539832
junheeLee96 <dlwnsgml203@naver.com>
2023-09-20 22:47:54 +0900

    test 2 fail

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,70 @@
-import React from "react";
+import React, { useEffect } from "react";
+import "./App.css";
+
+const values = [
+  {
+    src: "https://source.unsplash.com/random/sun",
+    name: "sun !!",
+  },
+  {
+    src: "https://source.unsplash.com/random/sun",
+    name: "sun !!",
+  },
+  {
+    src: "https://source.unsplash.com/random/sun",
+    name: "sun !!",
+  },
+  {
+    src: "https://source.unsplash.com/random/sun",
+    name: "sun !!",
+  },
+  {
+    src: "https://source.unsplash.com/random/sun",
+    name: "sun !!",
+  },
+  {
+    src: "https://source.unsplash.com/random/sun",
+    name: "sun !!",
+  },
+  {
+    src: "https://source.unsplash.com/random/sun",
+    name: "sun !!",
+  },
+];
 
 const App = () => {
-  return <div></div>;
+  useEffect(() => {
+    const Drag = () => {
+      console.log("drag!!");
+    };
+
+    const $wrap = document.querySelectorAll(".wrap");
+    console.log($wrap);
+
+    $wrap.forEach((el) => {
+      el.addEventListener("mousedown", Drag);
+    });
+
+    return () => {
+      $wrap.forEach((el) => {
+        el.removeEventListener("mousedown", Drag);
+      });
+    };
+  }, []);
+
+  return (
+    <div className="app">
+      <div className="container">
+        {values.map((v) => (
+          <div className="wrap">
+            <img src={v.src} style={{ width: "90px" }} alt="" />{" "}
+            <div>{v.name}</div>
+          </div>
+        ))}
+      </div>
+      <div className="container">g</div>
+    </div>
+  );
 };
 
 export default App;
